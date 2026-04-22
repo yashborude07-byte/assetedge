671572c4586689b0b5e9c16d72b531a4169823ca
yashborude07-byte <yashborude07@gmail.com>
2026-04-22 20:08:56 +0530

    Fix Vercel handler in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,23 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import io
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from assetedge_final.backend import app as application
+from assetedge_final.backend import app as flask_app
 
-# Vercel Python serverless
+def handler(request):
+    """Vercel Python handler"""
+    path = request['path']
+    method = request['method']
+    
+    with flask_app.test_request_context(path, method=method):
+        flask_app.preprocess_request()
+        resp = flask_app.handle_request()
+    
+    return {
+        'statusCode': resp.status_code,
+        'headers': dict(resp.headers),
+        'body': resp.get_data(as_text=True)
+    }
+
 
