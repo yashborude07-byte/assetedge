d050b5a7065a6ebcb852d33f72fd8fa961616496
yashborude07-byte <yashborude07@gmail.com>
2026-04-22 20:10:42 +0530

    Improve Vercel handler with proper FlaskRequest

--- api/index.py
+++ api/index.py
@@ -7,17 +7,29 @@ from assetedge_final.backend import app as flask_app
 
 def handler(request):
     """Vercel Python handler"""
-    path = request['path']
-    method = request['method']
+    from flask import Request as FlaskRequest
+    from werkzeug.wrappers import Response
     
-    with flask_app.test_request_context(path, method=method):
+    # Create Flask request
+    flask_req = FlaskRequest.from_environ({
+        'PATH_INFO': request.get('path', '/'),
+        'REQUEST_METHOD': request.get('method', 'GET'),
+        'HTTP_HOST': request.get('headers', {}).get('host', 'localhost'),
+        'wsgi.input': io.BytesIO(request.get('body', b'') or b''),
+        **{k.lower().replace('-', '_'): v for k, v in (request.get('headers', {}) or {}).items()}
+    })
+    
+    # Dispatch through Flask app
+    with flask_app.test_request_context(flask_req.path, flask_req):
         flask_app.preprocess_request()
-        resp = flask_app.handle_request()
+        rv = flask_app.full_dispatch_request()
+        response = flask_app.make_response(rv)
     
     return {
-        'statusCode': resp.status_code,
-        'headers': dict(resp.headers),
-        'body': resp.get_data(as_text=True)
+        'statusCode': response.status_code,
+        'headers': dict(response.headers),
+        'body': response.get_data(as_text=True)
     }
+
 
 
